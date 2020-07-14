1bb23cd1517ce27c6b9463d6dbac95cb642e8475
Carinaemerim <carinaemerim2@gmail.com>
2020-07-14 20:11:14 -0300

    Refatora mensagens

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -44,7 +44,8 @@ insert into animal_color(id, name, label, weight) values
 INSERT into user(id, username, password, active, role, name, email, address, zip_code, neighborhood, city, state,
 address_number, residential_phone, cel_phone, animal_age_id, animal_gender_id, animal_type_id, animal_castrated_id,
 animal_color_id,animal_size_id, avatar) VALUES
-(100, 'user', @PASSWORD, 'true', 'ROLE_USER' ,'Baby Yoda','yoda@stars.wars', 'Garro', '92032380', 'Tattooine', 'StarWars', 'RS' ,544,'51982656565', '', 101, 102,101,101,101,101, 'default'),
+(103, 'user', @PASSWORD, 'true', 'ROLE_USER' ,'Baby Yoda','yoda@stars.wars', 'Garro', '92032380', 'Tattooine', 'StarWars', 'RS' ,544,'51982656565', '', 101, 102,101,101,101,101, 'default'),
+(100, 'r2d2', @PASSWORD, 'true', 'ROLE_USER' ,'R2d2','yoda@stars.wars', 'Garro', '92032380', 'Tattooine', 'StarWars', 'RS' ,544,'51982656565', '', 101, 102,101,101,101,101, 'default'),
 (101, 'admin', @PASSWORD, 'true', 'ROLE_ADMIN' ,'Admin Yoda','admin@stars.wars', 'Garro', '92032380', 'Tattooine', 'StarWars', 'RS' ,544,'51982656565', '', 101, 102,101,101,101,101, 'default'),
 (102, 'mod', @PASSWORD, 'true', 'ROLE_MODERATOR' ,'Mod Yoda','mod@stars.wars', 'Garro', '92032380', 'Tattooine', 'StarWars', 'RS' ,544,'51982656565', '', 101, 102,101,101,101,101, 'default');
 
